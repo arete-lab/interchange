5f204a3dee74bf2d11a4a34d1977cb7be065bc68
Francesco De Falco <defalco.fnc@gmail.com>
2023-06-07 07:32:20 +0200

    Added logic to existing order book
    add the logic to check for an existing order book for a particular pair of denoms

--- x/dex/keeper/msg_server_create_pair.go
+++ x/dex/keeper/msg_server_create_pair.go
@@ -2,16 +2,25 @@ package keeper
 
 import (
 	"context"
+	"errors"
+
+	"interchange/x/dex/types"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	clienttypes "github.com/cosmos/ibc-go/v6/modules/core/02-client/types"
-	"interchange/x/dex/types"
 )
 
 func (k msgServer) SendCreatePair(goCtx context.Context, msg *types.MsgSendCreatePair) (*types.MsgSendCreatePairResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: logic before transmitting the packet
+	// Get an order book index
+	pairIndex := types.OrderBookIndex(msg.Port, msg.ChannelID, msg.SourceDenom, msg.TargetDenom)
+
+	// If an order book is found, return an error
+	_, found := k.GetSellOrderBook(ctx, pairIndex)
+	if found {
+		return &types.MsgSendCreatePairResponse{}, errors.New("the pair already exist")
+	}
 
 	// Construct the packet
 	var packet types.CreatePairPacketData
@@ -20,7 +29,7 @@ func (k msgServer) SendCreatePair(goCtx context.Context, msg *types.MsgSendCreat
 	packet.TargetDenom = msg.TargetDenom
 
 	// Transmit the packet
-	_, err := k.TransmitCreatePairPacket(
+	err := k.TransmitCreatePairPacket(
 		ctx,
 		packet,
 		msg.Port,
